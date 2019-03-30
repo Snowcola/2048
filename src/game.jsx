d7d22ee5f4135e47649f8b30828385aacf817d81
Anthony Keelan <anthony.keelan@gmail.com>
2019-03-30 01:27:42 -0600

    style button

--- src/game.jsx
+++ src/game.jsx
@@ -1,35 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Mousetrap from 'mousetrap';
+import Mousetrap from "mousetrap";
 
-import Board from './Components/Board';
-import Cell from './cell';
-import { addRandom, moveLeft } from './gameLogic';
-import { clone, rotate } from './utils';
+import Board from "./Components/Board";
+import Button from "./Components/Button";
+import { addRandom, moveLeft } from "./gameLogic";
+import { clone, rotate } from "./utils";
 
-import './App.css';
+import "./App.css";
 
-//const initial_state = [[2, 2, 2, 2], [0, 2, 6, 0], [4, 4, 7, 0], [0, 4, 8, 0]];
 const initial_state = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
 
 export default function Game() {
   const [grid, setGrid] = useState(setup());
   const [mergedTiles, setMergedTiles] = useState([]);
+  const [score, setScore] = useState(0);
+  const [points, setPoints] = useState(0);
   const [old, setOld] = useState([]);
   const size = 4;
   const tileSize = 100;
   const tilepadding = 5;
 
   useEffect(() => {
-    Mousetrap.bind(['left', 'a'], () => moveDirection(0));
-    Mousetrap.bind(['up', 'w'], () => moveDirection(1));
-    Mousetrap.bind(['right', 'd'], () => moveDirection(2));
-    Mousetrap.bind(['down', 's'], () => moveDirection(3));
+    setScore(score + points);
+  }, [points]);
+
+  useEffect(() => {
+    Mousetrap.bind(["left", "a"], () => moveDirection(0));
+    Mousetrap.bind(["up", "w"], () => moveDirection(1));
+    Mousetrap.bind(["right", "d"], () => moveDirection(2));
+    Mousetrap.bind(["down", "s"], () => moveDirection(3));
     return () => {
-      Mousetrap.unbind(['left', 'a']);
-      Mousetrap.unbind(['up', 'w']);
-      Mousetrap.unbind(['right', 'd']);
-      Mousetrap.unbind(['down', 's']);
+      Mousetrap.unbind(["left", "a"]);
+      Mousetrap.unbind(["up", "w"]);
+      Mousetrap.unbind(["right", "d"]);
+      Mousetrap.unbind(["down", "s"]);
     };
   });
 
@@ -38,16 +43,14 @@ export default function Game() {
 
     for (let row = 0; row < matrix.length; row++) {
       for (let col = 0; col < temp[row].length; col++) {
-        if (typeof temp[row][col] == 'object') {
+        if (typeof temp[row][col] == "object") {
           temp[row][col].oldRow = temp[row][col].row;
           temp[row][col].oldCol = temp[row][col].col;
           temp[row][col].row = row;
           temp[row][col].col = col;
-          //console.log(temp[row][col]);
         }
       }
     }
-    console.log('running');
     return temp;
   }
 
@@ -66,8 +69,8 @@ export default function Game() {
     for (let i = 0; i < dirNum; i++) {
       matrix = rotate(matrix, true);
     }
-    matrix = moveLeft(matrix, setMergedTiles);
-    console.log('matrix', matrix, 'leftover', leftOver);
+    matrix = moveLeft(matrix, setMergedTiles, setPoints);
+
     for (let i = 0; i < dirNum; i++) {
       matrix = rotate(matrix, false);
     }
@@ -78,7 +81,9 @@ export default function Game() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Grid Test</h1>
+        <h1>2048</h1>
+        <h1>Score: {score}</h1>
+        <Button click={reset}>Reset</Button>
 
         <Board
           grid={grid}
@@ -87,10 +92,7 @@ export default function Game() {
           tilepadding={tilepadding}
           size={size}
         />
-
-        <button onClick={reset}>Reset</button>
       </header>
     </div>
   );
 }
-//export default App;
